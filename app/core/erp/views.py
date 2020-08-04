877d7e18e62fd9d3eda10550d9203757c2868df2
Ricardo <arpkumul@gmail.com>
2020-08-04 14:36:53 -0500

    primeras urls d prueba

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+def myfirstview(request):
+    data = {
+    'name': 'Alex'
+    }
+    return JsonResponse(data)
